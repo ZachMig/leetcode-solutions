e9a542fad8ae90247b1b8d88bd924dadaaf536d8
Zach Migliorini <zmigliorini@gmail.com>
2023-07-27 22:35:51 -0400

    Create OneFortyTwoLinkedListCycleII.java

---
+++ OneFortyTwoLinkedListCycleII.java
@@ -0,0 +1,38 @@
+/**
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer.
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed).
+It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        Set<ListNode> seen = new HashSet<>();
+
+        while (head != null) {
+            if (!seen.contains(head))
+                seen.add(head);
+            else
+                return head;
+            head = head.next;
+        }
+
+        return null;
+
+    }
+}
